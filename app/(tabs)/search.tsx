299957c1853ba59a4bfa96269a64149b8a5eb986
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-07-26 11:45:47 +0200

    Dealing with searching things

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -59,7 +59,7 @@ const Search= () => {
                                     <SearchBar
                                         placeholder="Search..."
                                         value={searchQuery}
-                                        onChangeText={(text:string) => setSearchQuery()}
+                                        onChangeText={(text:string) => setSearchQuery}
                                     />
                                 </View>
 
@@ -92,6 +92,15 @@ const Search= () => {
                                     )
                                 }
                             </>
+                      }
+                      ListEmptyComponent={
+                          !moviesLoading && !moviesError ? (
+                              <View className="mt-10 px-5">
+                                  <Text className="text-center text-gray-500">
+                                      {searchQuery.trim() ? "No movies found" : "Search for a movie"}
+                                  </Text>
+                              </View>
+                          ): null
                       }
             />
 
---
+++ services/appwrite.ts
@@ -0,0 +1,7 @@
+
+const databaseId = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID;
+const collectionId = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID;
+
+export const updateSearchQueryCount = async (query: string, movie: Movie) => {
+
+}
